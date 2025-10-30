db7777ae91ab187f4e06c3566c170d4d34f8dc05
anaol216 <atinafuanaol@gmail.com>
2025-10-31 00:09:42 +0300

    Refactor Book admin registration to include custom ModelAdmin with list display, search, and filter options

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from .models import Book
 
+class BookAdmin(admin.ModelAdmin):
+    # Fields to display in list view
+    list_display = ('title', 'author', 'publication_year')
+    
+    # Add search functionality
+    search_fields = ('title', 'author')
+    
+    # Add filters in the sidebar
+    list_filter = ('publication_year',)
 
-admin.site.register(Book)
-
-
-# Register your models here.
+# Register the Book model with the custom admin
+admin.site.register(Book, BookAdmin)
