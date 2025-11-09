a907b6bdcd7d833d3b2d6ce2ba82144de30f085c
anaol216 <atinafuanaol@gmail.com>
2025-11-09 23:15:03 +0300

    modified:   django-models/LibraryProject/relationship_app/urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,15 +1,16 @@
 from .views import LibraryDetailView
 from .views import list_books
 from .views import Library
-from django.urls import path
-from authentication_app.views import register, user_login, user_logout    
+from django.urls import path, include
+from . import views   
+from django.contrib.auth.views import LoginView, LogoutView
 
 app_name = 'relationship_app'
 urlpatterns = [
     path('books/',list_books, name='list_books'),
     path('libraries/', Library.as_view(), name='library_list'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
-    path('register/', register, name='register'),
-    path('login/', user_login, name='login'),
-    path('logout/', user_logout, name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logged_out.html'), name='logout'),
 ]
