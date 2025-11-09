316a60b580263fac4584b2bd36aecfb51fb40488
anaol216 <atinafuanaol@gmail.com>
2025-11-09 23:08:58 +0300

    modified:   django-models/LibraryProject/relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library, Book
 from django.views.generic.detail import DetailView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 def list_books(request):
     books = Book.objects.all()
