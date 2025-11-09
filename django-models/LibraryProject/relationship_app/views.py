ed8becb33b76397740174b96d5965c2804c91ffc
anaol216 <atinafuanaol@gmail.com>
2025-11-09 22:09:10 +0300

    modified:   django-models/LibraryProject/relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Library, Book
-from django.views.generic import ListView, DetailView
+from django.views.generic import DetailView, ListView
 
 # Create your views here.
 
