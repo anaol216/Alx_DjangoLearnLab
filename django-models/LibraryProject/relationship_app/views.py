a71bd08fc2c32f1a6f5116b0a247b7d5da923a8e
anaol216 <atinafuanaol@gmail.com>
2025-11-09 23:20:04 +0300

    modified:   django-models/LibraryProject/relationship_app/views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -5,6 +5,18 @@ from django.views.generic.detail import DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  
+            return redirect('login')  
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})
+
 def list_books(request):
     books = Book.objects.all()
     context = {'books': books}
