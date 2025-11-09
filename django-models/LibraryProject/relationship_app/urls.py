d2de1a8c4f0dd2e2e5dc0becbea9459b24cb8970
anaol216 <atinafuanaol@gmail.com>
2025-11-09 23:04:19 +0300

    Add login, logout, and register HTML templates; update URLs for authentication

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,11 +1,15 @@
 from .views import LibraryDetailView
 from .views import list_books
 from .views import Library
-from django.urls import path   
+from django.urls import path
+from authentication_app.views import register, user_login, user_logout    
 
 app_name = 'relationship_app'
 urlpatterns = [
     path('books/',list_books, name='list_books'),
     path('libraries/', Library.as_view(), name='library_list'),
     path('libraries/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
+    path('login/', user_login, name='login'),
+    path('logout/', user_logout, name='logout'),
 ]
