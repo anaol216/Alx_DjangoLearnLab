251e269562d1082f2a16302a7fe14ce6e373bb77
anaol216 <atinafuanaol@gmail.com>
2025-11-16 18:47:42 +0300

    modified:   advanced_features_and_security/LibraryProject/bookshelf/models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -7,7 +7,7 @@ class Book(models.Model):
     title= models.CharField(max_length=200)
     author= models.CharField(max_length=100)
     publication_year= models.IntegerField()
-class   CustomUserManager(BaseUserManager):
+class CustomUserManager(BaseUserManager):
     def create_user(self, username, password, **extra_fields):
         if not username:
             raise ValueError('The Username field must be set')
