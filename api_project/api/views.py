59a9639dff892e570dfd10bbca969c7b4cc3821b
anaol216 <atinafuanaol@gmail.com>
2025-11-23 09:40:50 +0300

    feat: modified the file

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -8,6 +8,6 @@ from .models import Book
 from .serializers import BookSerializer
 from rest_framework import generics
 
-class BookList(generics.ListApiView):
+class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
