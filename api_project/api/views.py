9b781d8aa817c334dba6875e9502fd85ed808dfd
anaol216 <atinafuanaol@gmail.com>
2025-11-23 10:55:40 +0300

    feat: using DFT authentications

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,18 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 from rest_framework import viewsets
 from .models import Book 
 
 from .serializers import BookSerializer
 from rest_framework import generics, viewsets
 
-class BookList(rest_framework.generics.ListAPIView):
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+
+class BookList(generics.ListAPIView):
+
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 
-class BookViewSet(rest_framework.viewsets.ModelViewSet):   
+
+class BookViewSet(viewsets.ModelViewSet):
+
+     authentication_classes = [TokenAuthentication]
+     permission_classes = [IsAuthenticated, IsAdminUser]
+
      queryset = Book.objects.all()  
      serializer_class = BookSerializer   
