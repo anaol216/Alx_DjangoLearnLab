119a938980e7876ed299b9d217e5b85782d385ec
anaol216 <atinafuanaol@gmail.com>
2025-11-23 09:46:59 +0300

    feat: used  rest_framework.viewsets.ModelViewSet

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -6,8 +6,13 @@ from rest_framework import viewsets
 from .models import Book 
 
 from .serializers import BookSerializer
-from rest_framework import generics
+from rest_framework import generics, viewsets
 
 class BookList(rest_framework.generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+
+
+class BookViewSet(rest_framework.viewsets.ModelViewSet):   
+     queryset = Book.objects.all()  
+     serializer_class = BookSerializer   
